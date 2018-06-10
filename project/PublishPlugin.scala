c8728c31a16b61386aa4f67c34bfad77830c687f
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-06-11 00:34:24 +0200

    s/property/env/

--- project/PublishPlugin.scala
+++ project/PublishPlugin.scala
@@ -8,16 +8,24 @@ object PublishPlugin extends AutoPlugin {
 
   override def trigger = allRequirements
   override def requires = JvmPlugin
+
   object autoImport {
     def isTravisTag: Boolean =
-      System.getProperty("TRAVIS_TAG") != null
+      System.getenv("TRAVIS_TAG") != null
+    def isTravisSecure: Boolean =
+      System.getenv("TRAVIS_SECURE_ENV_VARS") == "true"
   }
+
   import autoImport._
+
+  private def env(key: String): String =
+    Option(System.getenv(key)).getOrElse {
+      throw new NoSuchElementException(key)
+    }
 
   override def globalSettings: Seq[Def.Setting[_]] = List(
     organization := "com.geirsson",
     homepage := Some(url("https://github.com/scalameta/sbt-scalafmt")),
-    publishMavenStyle := true,
     licenses := Seq(
       "Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")
     ),
@@ -29,20 +37,33 @@ object PublishPlugin extends AutoPlugin {
         url("https://geirsson.com")
       )
     ),
+    publishMavenStyle := true,
     commands += Command.command("ci-release") { s =>
-      println("Setting up gpg")
+      val logger = streams.value.log
+      logger.info(
+        s"Running ci-release.\n" +
+          s"  TRAVIS_SECURE_ENV_VARS=${env("TRAVIS_SECURE_ENV_VARS")}\n" +
+          s"  TRAVIS_BRANCH=${env("TRAVIS_BRANCH")}\n" +
+          s"  TRAVIS_TAG=${env("TRAVIS_TAG")}"
+      )
       "git log HEAD~2..HEAD".!
-      (s"echo ${sys.env("PGP_SECRET")}" #| "base64 --decode" #| "gpg --import").!
 
-      if (!isTravisTag) {
-        println(s"No tag push, publishing a SNAPSHOT")
-        "+publish" ::
-          s
+      if (!isTravisSecure) {
+        logger.info("No access to secret variables, doing nothing")
+        s
       } else {
-        println("Tag pus detected, publishing a stable release")
-        "+publishSigned" ::
-          "sonatypeReleaseAll" ::
-          s
+        logger.info("Setting up gpg")
+        (s"echo ${env("PGP_SECRET")}" #| "base64 --decode" #| "gpg --import").!
+        if (!isTravisTag) {
+          logger.info(s"No tag push, publishing SNAPSHOT")
+          "+publish" ::
+            s
+        } else {
+          logger.info("Tag push detected, publishing a stable release")
+          "+publishSigned" ::
+            "sonatypeReleaseAll" ::
+            s
+        }
       }
     }
   )
