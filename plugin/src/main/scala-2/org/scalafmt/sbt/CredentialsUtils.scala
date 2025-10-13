c75d374707444e8d9b4f9468f661b334bf6e13f4
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-10-13 10:22:48 -0700

    formatter: update to 3.10.0, adjust imports (#408)

--- plugin/src/main/scala-2/org/scalafmt/sbt/CredentialsUtils.scala
+++ plugin/src/main/scala-2/org/scalafmt/sbt/CredentialsUtils.scala
@@ -1,7 +1,6 @@
 package org.scalafmt.sbt
 
-import sbt.librarymanagement.ivy.Credentials
-import sbt.librarymanagement.ivy.DirectCredentials
+import sbt.librarymanagement.ivy.{Credentials, DirectCredentials}
 
 object CredentialsUtils {
   def toDirect(c: Credentials): DirectCredentials = Credentials.toDirect(c)
